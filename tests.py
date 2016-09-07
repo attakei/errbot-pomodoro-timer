55d58dbf3a8a77946114e09315d1b0eb6ebb2389
attakei <attakei@gmail.com>
2016-09-08 03:41:42 +0900

    Add test case

--- tests.py
+++ tests.py
@@ -1,10 +1,22 @@
 import os
-from errbot.backends.test import testbot
+from errbot.backends.test import testbot, FullStackTest
+from errbot.backends.base import Person
 
 
 class PomodoroPluginTests(object):
     extra_plugin_dir = '.'
 
-    def test_command(self, testbot):
+    def fetch_plugin(self, testbot):
+        return testbot.bot.plugin_manager.get_plugin_obj_by_name('Pomodoro')
+
+    def test_start(self, testbot):
         testbot.push_message('!pomodoro_start')
         assert 'Start timer' in testbot.pop_message()
+        plugin = self.fetch_plugin(testbot)
+        assert plugin._timer[0] == 0
+        assert isinstance(plugin._timer[1], Person) is True
+
+    def test_stop_not_started(self, testbot):
+        testbot.push_message('!pomodoro_stop')
+        plugin = self.fetch_plugin(testbot)
+        assert plugin._timer == [None, None]
